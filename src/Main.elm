ad28d6a984b87583379e2b674f5794d9e9cea9bd
rsninja722 <jnick722547@gmail.com>
2022-03-17 13:11:24 -0400

    style changes and input limits
    changed button and input style, added units next to input, resized and moved text

--- src/Main.elm
+++ src/Main.elm
@@ -3,79 +3,78 @@ module Main exposing (..)
 import GraphicSVG exposing (..)
 import GraphicSVG.EllieApp exposing (..)
 
-import Html exposing (input)
-import Html.Attributes exposing (style, type_, value)
-import Html.Events exposing (onInput)
-import String exposing (fromFloat)
-import String exposing (fromInt)
+import Html exposing (input, button, text)
+import Html.Attributes exposing (style, type_, value, max, min)
+import Html.Events exposing (onInput, onClick)
+import String exposing (fromFloat, fromInt)
 
 
 myShapes model =
     [ titleText
-    , questionText model |> move ( 0, 30 )
-    , questionInput |> move ( -20, 30 )
-    , equationText |> move ( -17, 0 )
-    , resultText model
+    , questionText model |> move ( 0, 12 )
+    , questionInput |> move ( -32, -10 )
+    , equationText |> move ( -26, -40 )
+    , resultText model |> move ( -10, -5 )
     , bigGraph model |> move ( 5, -24 )
     , controls|> move ( 15, 0)
-    , text "Money X 1000" |> size 6 |> filled black |> rotate (degrees 90) |> move ( 0, -39 )
-    , text "Time" |> size 6 |> filled black |> move ( 40, -59 )
+    , GraphicSVG.text "Money X 1000" |> size 6 |> filled black |> rotate (degrees 90) |> move ( 0, -39 )
+    , GraphicSVG.text "Time" |> size 6 |> filled black |> move ( 40, -59 )
     ]
 
 
 titleText =
-    text "1MM3 - Interest Calulator" |> centered |> filled black |> move ( 0, 40 )
+    GraphicSVG.text "Interest Calulator" |> centered |> filled black |> move ( 0, 40 )
 
 
 questionText model =
     group
-        [ text "Suppose $                is invested at an annual interst rate" |> filled black |> scale 0.25 |> move ( -70, 0 )
-        , text "of                % for a period of                years. Compute" |> filled black |> scale 0.25 |> move ( -70, -5 )
-        , text "the future value of the investment if interest is" |> filled black |> scale 0.25 |> move ( -70, -10 )
-        , text "compunded continuosly:" |> filled black |> scale 0.25 |> move ( -70, -15 )
-        , text (fromFloat model.valP) |> filled blue |> scale 0.25 |> move ( -57, 0 )
-        , text (fromFloat model.valR) |> filled blue |> scale 0.25 |> move ( -65, -5 )
-        , text (fromFloat model.valT) |> filled blue |> scale 0.25 |> move ( -33, -5 )
+        [ GraphicSVG.text "Suppose $                is invested at an annual" |> filled black |> scale 0.35 |> move ( -80, 0 )
+        , GraphicSVG.text " interest rate of      % for a period" |> filled black |> scale 0.35 |> move ( -80, -5 )
+        , GraphicSVG.text "of          years. Compute the future value of " |> filled black |> scale 0.35 |> move ( -80, -10 )
+        , GraphicSVG.text "the investment if interest is compounded" |> filled black |> scale 0.35 |> move ( -80, -15 )
+        , GraphicSVG.text "continuously:" |> filled black |> scale 0.35 |> move ( -80, -20 )
+        , GraphicSVG.text (fromFloat model.valP) |> filled blue |> scale 0.35 |> move ( -62, 0 )
+        , GraphicSVG.text (fromFloat model.valR) |> filled blue |> scale 0.35 |> move ( -53, -5 )
+        , GraphicSVG.text (fromFloat model.valT) |> filled blue |> scale 0.35 |> move ( -75, -10 )
         ]
 
 
 questionInput =
     group
-        [ html 62 22 (input [ type_ "number", onInput UpdateP, value "1000", style "width" "56px", style "height" "16px", style "padding" "0" ] []) |> scale 0.25 |> move ( -15, -20 )
-        , html 62 22 (input [ type_ "number", onInput UpdateR, value "3", style "width" "56px", style "height" "16px", style "padding" "0" ] []) |> scale 0.25 |> move ( -15, -25 )
-        , html 62 22 (input [ type_ "number", onInput UpdateT, value "10", style "width" "56px", style "height" "16px", style "padding" "0" ] []) |> scale 0.25 |> move ( -15, -30 )
-        , text "P" |> filled blue |> scale 0.5 |> move ( -19, -25 )
-        , text "R" |> filled blue |> scale 0.5 |> move ( -19, -30 )
-        , text "T" |> filled blue |> scale 0.5 |> move ( -20, -35 )
-        , text "(principal)" |> filled black |> scale 0.25 |> move ( 1, -24 )
-        , text "(rate)" |> filled black |> scale 0.25 |> move ( 1, -29 )
-        , text "(time)" |> filled black |> scale 0.25 |> move ( 1, -34 )
+        [ html 62 22 (input [ style "border" "0.5px solid grey", style "box-shadow" "inset rgb(0 0 0 / 20%) -2px 2px 4px 0px", type_ "number", Html.Attributes.max "1000000", Html.Attributes.min "1", onInput UpdateP, value "1000", style "width" "56px", style "height" "16px", style "padding" "0" ] []) |> scale 0.25 |> move ( -15, -20 )
+        , html 62 22 (input [ style "border" "0.5px solid grey", style "box-shadow" "inset rgb(0 0 0 / 20%) -2px 2px 4px 0px", type_ "number", Html.Attributes.max "99", Html.Attributes.min "0", onInput UpdateR, value "3", style "width" "56px", style "height" "16px", style "padding" "0" ] []) |> scale 0.25 |> move ( -15, -25 )
+        , html 62 22 (input [ style "border" "0.5px solid grey", style "box-shadow" "inset rgb(0 0 0 / 20%) -2px 2px 4px 0px", type_ "number", Html.Attributes.max "1000", Html.Attributes.min "1", onInput UpdateT, value "10", style "width" "56px", style "height" "16px", style "padding" "0" ] []) |> scale 0.25 |> move ( -15, -30 )
+        , GraphicSVG.text "P" |> filled blue |> scale 0.5 |> move ( -19, -25 )
+        , GraphicSVG.text "R" |> filled blue |> scale 0.5 |> move ( -19, -30 )
+        , GraphicSVG.text "T" |> filled blue |> scale 0.5 |> move ( -20, -35 )
+        , GraphicSVG.text "$ (principal)" |> filled black |> scale 0.25 |> move ( 1, -24 )
+        , GraphicSVG.text "% (rate)" |> filled black |> scale 0.25 |> move ( 1, -29 )
+        , GraphicSVG.text "years (time)" |> filled black |> scale 0.25 |> move ( 1, -34 )
         ]
 
 
 equationText =
     group
-        [ text "B(t) = Pe" |> customFont "Georgia,'Times New Roman',Times,serif" |> italic |> filled black |> scale 0.5 |> move ( -54, 0 )
-        , text "rt" |> customFont "Georgia,'Times New Roman',Times,serif" |> italic |> filled black |> scale 0.25 |> move ( -30, 3 )
+        [ GraphicSVG.text "B(t) = Pe" |> customFont "Georgia,'Times New Roman',Times,serif" |> italic |> filled black |> scale 0.5 |> move ( -54, 0 )
+        , GraphicSVG.text "rt" |> customFont "Georgia,'Times New Roman',Times,serif" |> italic |> filled black |> scale 0.25 |> move ( -30, 3 )
         ]
 
 
 resultText model =
-    text ("result after " ++ fromFloat model.valT ++ " years: " ++ fromInt (round model.result)) |> filled black |> scale 0.5 |> move ( -70, -50 )
+    GraphicSVG.text ("result after " ++ fromFloat model.valT ++ " years: $" ++ fromInt (round model.result)) |> filled black |> scale 0.5 |> move ( -70, -50 )
 
 controls =
     group
         [
         group
-            [ circle 5 |> filled red |> move ( -40, 0 ) |> notifyTap Grid
-            , text "Grid" |> filled red |> move ( -72, 0 )
+            [
+                html 50 30 (button [onClick Grid, style "position" "absolute", style "left" "1px", style "top" "1px",  style "box-shadow" "0px 0.5px 2px -1px rgb(0 0 0 / 30%), 0px 2px 2px 0px rgb(0 0 0 / 20%), 0px 1px 2px 0px rgb(0 0 0 / 15%)"] [Html.text "Grid"]) |> scale 0.5
             ]
-            |> move ( 80, -20 )
+            |> move ( 30, -15 )
         , group
-            [ circle 5 |> filled blue |> move ( -40, -20 ) |> notifyTap Axes
-            , text "Axes" |> filled blue |> move ( -72, -20 )
+            [ html 50 30 (button [onClick Axes , style "position" "absolute", style "left" "1px", style "top" "1px",  style "box-shadow" "0px 0.5px 2px -1px rgb(0 0 0 / 30%), 0px 2px 2px 0px rgb(0 0 0 / 20%), 0px 1px 2px 0px rgb(0 0 0 / 15%)"] [Html.text "Axes"]) |> scale 0.5 
             ]
-            |> move ( 130, 0 )
+            |> move ( 70, -15 )
         ]
         |> move ( -25, 30 )
 
@@ -109,7 +108,7 @@ bigGraph model = group
       |> List.map ( \ idx -> 
                     let
                       x =  2* toFloat idx
-                    in [ text (String.fromInt idx)
+                    in [ GraphicSVG.text (String.fromInt idx)
                               |> centered
                               |>bold
                               |> (if (model.axes) then (filled black) else (filled white))
@@ -128,7 +127,7 @@ bigGraph model = group
       |> List.map ( \ idx -> 
                     let
                       y = 10*toFloat  idx
-                    in [ text (String.fromInt idx)
+                    in [ GraphicSVG.text (String.fromInt idx)
                               |> bold
                               |> (if (model.axes) then (filled black) else (filled white))
                               |> scale 0.5
@@ -180,7 +179,12 @@ type alias Model =
 update msg model =
     case msg of
         Tick t _ ->
-            { model | time = t }
+            { model | time = t
+                , valP = if model.valP > 1000000 then  1000000 else (if model.valP < 1 then 1 else model.valP)
+                , valR = if model.valR > 99 then  99 else (if model.valR < 0 then 0 else model.valR)
+                , valT = if model.valT > 1000 then 1000 else (if model.valT < 1 then 1 else model.valT)
+                , result = model.valP * (e ^ ((model.valR / 100) * model.valT))
+            }
 
         UpdateP txt ->
             { model | valP = removeMaybe (String.toFloat txt), result = removeMaybe (String.toFloat txt) * (e ^ ((model.valR / 100) * model.valT)) }
